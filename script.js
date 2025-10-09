5a771b5f63c46f54625b81c1f9e39dbb02104783
drstopone <119494552+drstopone@users.noreply.github.com>
2025-10-09 20:11:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ async function sendMessage() {
 function addMessage(text, sender) {
     const chatContainer = document.getElementById('chatContainer');
     const messageDiv = document.createElement('div');
-    messageDiv.className = message {sender}-message;
+    messageDiv.className = 'message ${sender}-message';
     messageDiv.textContent = text;
     chatContainer.appendChild(messageDiv);
     chatContainer.scrollTop = chatContainer.scrollHeight;
